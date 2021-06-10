576b469abe03bfb05adddf5264768881f20806fb
scripka <olga.morgan17@gmail.com>
2021-06-10 02:32:10 -0600

    Create a component that combines all items of homepage

--- src/features/HomePage/HomePage.js
+++ src/features/HomePage/HomePage.js
@@ -1,11 +1,23 @@
 import HeaderForm from '../HeaderForm/HeaderForm'
 import RepoList from '../RepoList/RepoList'
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";
 
-const HomePage = () => {
+const HomePage = (props) => {
+
+  const dispatch = useDispatch();
+  const repos = useSelector(state => state.repos);
+  const [reposList, setReposList] = useState(null)
+
+  useEffect( async ()=> {
+    await repos;
+  }, [repos])
   return(
     <section>
       <HeaderForm/>
-      <RepoList/>
+    {/* {reposList && */}
+    <RepoList/>
+    {/* } */}
     </section>
   )
 }
