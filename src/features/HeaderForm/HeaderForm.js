f07fffe67909d1a17da90518a72519f7fa007587
scripka <olga.morgan17@gmail.com>
2021-06-10 16:07:29 -0600

    Fix fetch requests, change queries

--- src/features/HeaderForm/HeaderForm.js
+++ src/features/HeaderForm/HeaderForm.js
@@ -2,8 +2,7 @@ import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
 import './HeaderForm.css'
-import { makeStyles } from '@material-ui/core/styles';
-import { fetchBegin, fetchFailure, fetchRepoData, setQueries } from '../../redux/actions';
+import { fetchBegin, fetchFailure, fetchRepoData, setQueries, setKeywords } from '../../redux/actions';
 import { baseUrl } from '../../apiCalls'
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
@@ -13,39 +12,33 @@ import FormControl from '@material-ui/core/FormControl';
 import FormHelperText from '@material-ui/core/FormHelperText';
 import Select from '@material-ui/core/Select';
 import Pagination from '@material-ui/lab/Pagination';
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    '& > *': {
-      marginTop: theme.spacing(2),
-    },
-  },
-}));
+import Loading from '../Loading/Loading';
+import ErrorPage from '../ErrorPage/ErrorPage';
 
 const HeaderForm = () => {
+  const [keywordInput, setKeywordInput] = useState('')
   const [query, setQuery] = useState({
-    keywords: '',
     sortType: '',
     language: '',
     page: 1
   });
-  const classes = useStyles();
+
   const [languages, setLanguages] = useState();
-  const [loading, setLoading] = useState(false);
   const dispatch = useDispatch();
   const repos = useSelector(state => state.repos);
-  const keywords = useSelector(state => state.keywords);
+  const keywords = useSelector(state => state.keyword);
+  const page = useSelector(state => state.page);
 
   //For the error catching and loading screens
-  // const loading = useSelector(state => state.loading);
-  // const error = useSelector(state => state.error);
+  const loading = useSelector(state => state.loading);
+  const error = useSelector(state => state.error);
 
-  const fetchData = async (page=1) => {
+  const fetchData = async () => {
     if(keywords) {
       let nameInput = !keywords ? '' : `&q=${keywords}`;
       let languageInput = !query.language ? '' : `+language:${query.language}`;
       let sortTypeInput = !query.sortType ? '' : `&sort=${query.sortType}`;
-      let pageInput = `&page=${query.page}`;
+      let pageInput = `&page=${page}`;
       let inputInfo = nameInput+languageInput+pageInput+sortTypeInput;
       dispatch(fetchBegin(true));
       await axios.get(`${baseUrl}search/repositories?&order=desc${inputInfo}`)
@@ -82,11 +75,10 @@ const HeaderForm = () => {
     e.preventDefault() 
     let inputPhrase = e.target.value;
     let associatedQuery = e.target.name;
-
     if(!inputPhrase) { 
-      setQuery({...query, [associatedQuery]: ''})
+      setKeywordInput('')
     }
-    setQuery({...query, [associatedQuery]: inputPhrase})
+    setKeywordInput(inputPhrase)
   }
 
   const handleSelectSort = async (e) => {
@@ -99,84 +91,105 @@ const HeaderForm = () => {
     const cleanQueries = () => {
       setQuery({...query,
         language: '',
+        sortType: '',
         page: 1
       })
       setLanguages(null)
     }
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    dispatch(setQueries(query));
-    if(!query.keywords) {
+    dispatch(setKeywords({keyword: keywordInput}));
+    if(!keywordInput) {
       dispatch(fetchRepoData([]))
+      dispatch(setKeywords(''))
       cleanQueries()
     }
     cleanQueries()
   }
+
   const handlePageChange = (e, value) => {
-    setQuery({...query, page: value})
+    setQuery({...query, page: value});
+    dispatch(setQueries({...query, page: value}));
   }
+ 
+  useEffect(() => {
+    if(keywords) {
+      fetchData()
+    }
+  }, [keywords])
 
   useEffect(() => {
-    fetchData()
+      fetchData()
   }, [query])
 
-  return (
-    <section className='header-container'>
-      <h1>RepoFinder</h1>
-      <form className="header-form" autoComplete="off" onSubmit={(e) => handleSubmit(e)}>
-          <TextField id="standard-search" onChange={(e) => handleChange(e)} name={"keywords"} label="Search..." type="search" />
-          <Button type="submit" color="primary">Submit</Button>
-      </form>
-      {!!repos.items &&
-        <div className="sort-filter-container">
-          <div>
-            <FormControl className="form filter-wrapper" onSubmit={(e) => handleSubmit(e)}>
-            <InputLabel id="languages"></InputLabel>
-            <Select
-              labelId="languages"
-              id="language"
-              name="language"
-              displayEmpty
-              value={query.language}
-              onChange={(e) => handleSelectSort(e)}
-            >
-              <MenuItem value="">
-                <em>All Languages</em>
-              </MenuItem>
-              {
-                languages &&
-                  Object.keys(languages).sort((a,b) => languages[b] - languages[a]).map(languageName => {
-                    return(
-                      <MenuItem key={`sort-${languageName}`}value={`${languageName}`}>{`${languageName}`}<span>{languages[languageName]}</span></MenuItem>
-                    )
-                  })
-              }
-            </Select>
-            <FormHelperText>Sort By</FormHelperText>
-            </FormControl>
-            <FormControl className="form sort-wrapper" onSubmit={(e) => handleSubmit(e)}>
-              <InputLabel id="sort"></InputLabel>
-              <Select
-                labelId="sort"
-                id="sortType"
-                name="sortType"
-                displayEmpty
-                value={query.sortType}
-                onChange={(e) => handleSelectSort(e)}
-              >
-                <MenuItem value="">
-                  <em>Best Match</em>
-                </MenuItem>
-                <MenuItem value={"stars"}>Stars</MenuItem>
-              </Select>
-              <FormHelperText>Sort By</FormHelperText>
-            </FormControl>
-          </div>
-          <Pagination count={repos.total_count > 1000 ? Math.floor(1000/30)+1 : Math.floor(repos.total_count/30)+1} page={query.page} onChange={handlePageChange} variant="outlined" shape="rounded" />
-        </div>
-      }
-    </section>
-  )
+  if(error) {
+    return <ErrorPage/>
+  } else {
+    return (
+      <>
+      {loading === false ? (
+        <section className='header-container'>
+          <h1>RepoFinder</h1>
+          <form className="header-form" autoComplete="off" onSubmit={(e) => handleSubmit(e)}>
+              <TextField id="standard-search" onChange={(e) => handleChange(e)} name={"keywords"} label="Search..." type="search" />
+              <Button type="submit" color="primary">Submit</Button>
+          </form>
+          {!!repos.items &&
+            <div className="sort-filter-container">
+              <div>
+                <FormControl className="form filter-wrapper" onSubmit={(e) => handleSubmit(e)}>
+                <InputLabel id="languages"></InputLabel>
+                <Select
+                  labelId="languages"
+                  id="language"
+                  name="language"
+                  displayEmpty
+                  value={query.language}
+                  onChange={(e) => handleSelectSort(e)}
+                >
+                  <MenuItem value="">
+                    <em>All Languages</em>
+                  </MenuItem>
+                  {
+                    languages &&
+                      Object.keys(languages).sort((a,b) => languages[b] - languages[a]).map(languageName => {
+                        return(
+                          <MenuItem className="menu-item" key={`sort-${languageName}`} value={`${languageName}`}>{`${languageName}`}<span className="filter-lan-num">{languages[languageName]}</span></MenuItem>
+                        )
+                      })
+                  }
+                </Select>
+                <FormHelperText>Sort By</FormHelperText>
+                </FormControl>
+                <FormControl className="form sort-wrapper" onSubmit={(e) => handleSubmit(e)}>
+                  <InputLabel id="sort"></InputLabel>
+                  <Select
+                    labelId="sort"
+                    id="sortType"
+                    name="sortType"
+                    displayEmpty
+                    value={query.sortType}
+                    onChange={(e) => handleSelectSort(e)}
+                  >
+                    <MenuItem value="">
+                      <em>Best Match</em>
+                    </MenuItem>
+                    <MenuItem value={"stars"}>Stars</MenuItem>
+                  </Select>
+                  <FormHelperText>Sort By</FormHelperText>
+                </FormControl>
+              </div>
+              <Pagination count={repos.total_count > 1000 ? Math.floor(1000/30)+1 : Math.floor(repos.total_count/30)+1} page={parseInt(page)} onChange={handlePageChange} variant="outlined" shape="rounded" />
+            </div>
+          }
+        </section>) : (
+          <Loading/>
+          )
+        }
+      </>
+    )
+    }
 }
 
 export default HeaderForm;
