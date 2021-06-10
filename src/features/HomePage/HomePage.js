53c48111f4c5723b3806c9ed2c1183183abb9e43
scripka <olga.morgan17@gmail.com>
2021-06-10 03:24:24 -0600

    Clean unsused code in Homepage

--- src/features/HomePage/HomePage.js
+++ src/features/HomePage/HomePage.js
@@ -1,23 +1,12 @@
 import HeaderForm from '../HeaderForm/HeaderForm'
 import RepoList from '../RepoList/RepoList'
-import { useDispatch, useSelector } from "react-redux";
-import { useEffect, useState } from "react";
 
 const HomePage = (props) => {
 
-  const dispatch = useDispatch();
-  const repos = useSelector(state => state.repos);
-  const [reposList, setReposList] = useState(null)
-
-  useEffect( async ()=> {
-    await repos;
-  }, [repos])
   return(
     <section>
       <HeaderForm/>
-    {/* {reposList && */}
-    <RepoList/>
-    {/* } */}
+      <RepoList/>
     </section>
   )
 }
