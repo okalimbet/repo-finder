7c8a856590263634e26201ed94b15023ef07cb05
scripka <olga.morgan17@gmail.com>
2021-06-10 03:23:58 -0600

    Add media queries, clean unused code

--- src/features/HeaderForm/HeaderForm.js
+++ src/features/HeaderForm/HeaderForm.js
@@ -1,7 +1,9 @@
 import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
 import './HeaderForm.css'
-import {fetchBegin, fetchFailure, fetchRepoData, setQueries} from '../../redux/actions';
+import { fetchBegin, fetchFailure, fetchRepoData, setQueries } from '../../redux/actions';
+import { baseUrl } from '../../apiCalls'
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import InputLabel from '@material-ui/core/InputLabel';
@@ -9,7 +11,6 @@ import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
 import FormHelperText from '@material-ui/core/FormHelperText';
 import Select from '@material-ui/core/Select';
-import axios from "axios";
 
 const HeaderForm = () => {
   const [query, setQuery] = useState({
@@ -19,74 +20,71 @@ const HeaderForm = () => {
     page: ''
   });
 
-const [languages, setLanguages] = useState()
-const [loading, setLoading] = useState(false)
+  const [languages, setLanguages] = useState();
+  const [loading, setLoading] = useState(false);
   const dispatch = useDispatch();
   const repos = useSelector(state => state.repos);
   const keywords = useSelector(state => state.keywords);
-  const sortType = useSelector(state => state.sortType);
+
+  //For the error catching and loading screens
   // const loading = useSelector(state => state.loading);
   // const error = useSelector(state => state.error);
-  const language = useSelector(state => state.language);
 
   const fetchData = async (page=1) => {
     if(keywords) {
       let nameInput = !keywords ? '' : `&q=${keywords}`;
       let languageInput = !query.language ? '' : `+language:${query.language}`;
       let sortTypeInput = !query.sortType ? '' : `&sort=${query.sortType}`;
-      let inputInfo = nameInput+languageInput+sortTypeInput
-    dispatch(fetchBegin(true));
-    await axios.get(`https://api.github.com/search/repositories?&order=desc&${inputInfo}`)
-      .then(res =>
-        {
-          dispatch(fetchRepoData(res.data))
-          if(!languages) {
-          handleLanguages(res.data)
+      let inputInfo = nameInput+languageInput+sortTypeInput;
+      dispatch(fetchBegin(true));
+      await axios.get(`${baseUrl}${inputInfo}`)
+        .then(res =>
+          {
+            dispatch(fetchRepoData(res.data))
+            if(!languages) {
+            handleLanguages(res.data)
+            }
           }
-        }
-      )
-      .catch(error => dispatch(fetchFailure(error)))
+        )
+        .catch(error => dispatch(fetchFailure(error)));
     } else {
-      return(<section>provide keyword</section>)
+      return;
     }
   }
   
   const handleLanguages = async (data) => {
-    console.log(data)
     if(data.items) {
-    const allLanguagesFromRepos = data.items.reduce((allLanguages, oneRepo) => {
-        if(!allLanguages[oneRepo.language] && oneRepo.language != null) {
-
-          allLanguages[oneRepo.language] = 0
-        };
-        if(oneRepo.language != null) {
-        allLanguages[oneRepo.language] += 1;
-        }
-      return allLanguages;
-    }, {})
-    await setLanguages(allLanguagesFromRepos)
-  }
+      const allLanguagesFromRepos = data.items.reduce((allLanguages, oneRepo) => {
+          if(!allLanguages[oneRepo.language] && oneRepo.language != null) {
+            allLanguages[oneRepo.language] = 0
+          };
+          if(oneRepo.language != null) {
+          allLanguages[oneRepo.language] += 1;
+          }
+        return allLanguages;
+      }, {})
+      await setLanguages(allLanguagesFromRepos)
+    }
   }
 
   const handleChange = (e) => {
     e.preventDefault() 
-    if(!e.target.value) {setQuery({...query, [e.target.name]: ''})}
     let inputPhrase = e.target.value;
-    
-    let formattedPhrase = inputPhrase.split(' ').join("+")
-    console.log(formattedPhrase)
-    setQuery({...query, [e.target.name]: inputPhrase})
+    let associatedQuery = e.target.name;
+
+    if(!inputPhrase) { 
+      setQuery({...query, [associatedQuery]: ''})
+    }
+    setQuery({...query, [associatedQuery]: inputPhrase})
   }
 
   const handleSelectSort = async (e) => {
     e.preventDefault();
-    console.log("target " + e.target.value)
-    setQuery({...query, [e.target.name]: e.target.value});
-    // dispatch(setQueries(query));
-    }
-    // console.log(query)
-    // fetchData()
-  // }
+    let inputFilter = e.target.value;
+    let associatedQuery = e.target.name;
+    setQuery({...query, [associatedQuery]: inputFilter});
+  }
+
     const cleanQueries = () => {
       setQuery({...query,
         language: ''
@@ -102,17 +100,10 @@ const [loading, setLoading] = useState(false)
     }
     cleanQueries()
   }
-  
-useEffect(async() => {
-   fetchData()
-   await repos
-}, [keywords])
 
-useEffect(() => {
-  // let infoData = `&q=${keywords}+language:${language}&sort=${sortType}`
-   fetchData()
-  //  await repos
-}, [query])
+  useEffect(() => {
+    fetchData()
+  }, [query])
 
   return (
     <section className='header-container'>
@@ -122,49 +113,49 @@ useEffect(() => {
           <Button type="submit" color="primary">Submit</Button>
       </form>
       {!!repos.items &&
-      <>
-        <FormControl onSubmit={(e) => handleSubmit(e)}>
-        <InputLabel id="languages"></InputLabel>
-        <Select
-          labelId="languages"
-          id="language"
-          name="language"
-          displayEmpty
-          value={query.language}
-          onChange={(e) => handleSelectSort(e)}
-        >
-          <MenuItem value="">
-            <em>All Languages</em>
-          </MenuItem>
-          {
-           languages &&
-            Object.keys(languages).sort((a,b) => languages[b] - languages[a]).map(languageName => {
-              return(
-                <MenuItem key={`sort-${languageName}`}value={`${languageName}`}>{`${languageName}`}<span>{languages[languageName]}</span></MenuItem>
-              )
-            })
-          }
-        </Select>
-        <FormHelperText>Sort By</FormHelperText>
-      </FormControl>
-      <FormControl onSubmit={(e) => handleSubmit(e)}>
-        <InputLabel id="sort"></InputLabel>
-        <Select
-          labelId="sort"
-          id="sortType"
-          name="sortType"
-          displayEmpty
-          value={query.sortType}
-          onChange={(e) => handleSelectSort(e)}
-        >
-          <MenuItem value="">
-            <em>Best Match</em>
-          </MenuItem>
-          <MenuItem value={"stars"}>Stars</MenuItem>
-        </Select>
-        <FormHelperText>Sort By</FormHelperText>
-      </FormControl>
-      </>
+        <div className="sort-filter-container">
+          <FormControl className="form filter-wrapper" onSubmit={(e) => handleSubmit(e)}>
+          <InputLabel id="languages"></InputLabel>
+          <Select
+            labelId="languages"
+            id="language"
+            name="language"
+            displayEmpty
+            value={query.language}
+            onChange={(e) => handleSelectSort(e)}
+          >
+            <MenuItem value="">
+              <em>All Languages</em>
+            </MenuItem>
+            {
+              languages &&
+                Object.keys(languages).sort((a,b) => languages[b] - languages[a]).map(languageName => {
+                  return(
+                    <MenuItem key={`sort-${languageName}`}value={`${languageName}`}>{`${languageName}`}<span>{languages[languageName]}</span></MenuItem>
+                  )
+                })
+            }
+          </Select>
+          <FormHelperText>Sort By</FormHelperText>
+          </FormControl>
+          <FormControl className="form sort-wrapper" onSubmit={(e) => handleSubmit(e)}>
+            <InputLabel id="sort"></InputLabel>
+            <Select
+              labelId="sort"
+              id="sortType"
+              name="sortType"
+              displayEmpty
+              value={query.sortType}
+              onChange={(e) => handleSelectSort(e)}
+            >
+              <MenuItem value="">
+                <em>Best Match</em>
+              </MenuItem>
+              <MenuItem value={"stars"}>Stars</MenuItem>
+            </Select>
+            <FormHelperText>Sort By</FormHelperText>
+          </FormControl>
+        </div>
       }
     </section>
   )
