8d3c16e89e01fdb8cc63937b986ac47084133ee4
scripka <olga.morgan17@gmail.com>
2021-06-10 16:09:37 -0600

    Add loading and error pages, fix styleing

--- src/features/SingleRepoPreview/SingleRepoPreview.js
+++ src/features/SingleRepoPreview/SingleRepoPreview.js
@@ -10,6 +10,8 @@ import GitHubIcon from '@material-ui/icons/GitHub';
 import StarIcon from '@material-ui/icons/Star';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
+import Loading from '../Loading/Loading'
+import ErrorPage from '../ErrorPage/ErrorPage';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -17,20 +19,23 @@ const useStyles = makeStyles((theme) => ({
     flexDirection: 'column',
     justifyContent: 'space-around',
     flexWrap: 'wrap',
-    width: '94%',
+    width: '90%',
     height: 'fit-content',
     margin: '2em 0',
-    padding: '3%'
+    padding: '5%'
   },
 }));
 
 
 const SingleRepoPreview = () => {
+
+  const classes = useStyles();
+  const dispatch = useDispatch();
   const owner = useSelector(state => state.owner);
   const repoName = useSelector(state => state.repoName);
   const repoDetails = useSelector(state => state.repoDetails);
-  const classes = useStyles();
-  const dispatch = useDispatch();
+  const loading = useSelector(state => state.loading);
+  const error = useSelector(state => state.error);
 
   const fetchSingleRepo = async (ownerInfo, repoInfo) => {
     if(owner && repoName) {
@@ -49,6 +54,7 @@ const SingleRepoPreview = () => {
 
   const handleReturnClick = () => {
     dispatch(setUserData({owner: '', repoName: ''}))
+    dispatch(fetchRepoDataDetails(null))
   }
 
   useEffect(() => {
@@ -58,34 +64,44 @@ const SingleRepoPreview = () => {
     return dispatch(fetchRepoDataDetails(null))
   }, [owner])
 
+  if(error) {
+    return <ErrorPage/>
+  }
+
   if(repoDetails) {
   return(
-    <section className="single-repo-container">
-      <div>
-        <div className="single-repo-wrapper">
-          <h2 className="repo-fullname">{repoDetails.full_name}</h2>
-          <Paper className={classes.root} elevation={3}>
-              <Typography className="repo-owner"  color="textSecondary" gutterBottom>author: {repoDetails.owner.login}</Typography>
-              {repoDetails.description &&
-                <Typography className="repo-description" variant="body2" component="p">{repoDetails.description}</Typography>
-              }
-              <div className="stargazing-container">
-                <div className="stargazing-box">
-                  <StarIcon/>
-                  <Typography className="repo-details" variant="body2" component="p">{repoDetails.stargazers_count}</Typography>
-                </div>
-                <Typography className="repo-details" variant="body2" component="p">Language: {repoDetails.language}</Typography>
-              </div> 
-              <div className="github-icon">
-                <GitHubIcon/>
-              </div>       
-          </Paper>
-        </div>
-        <div className="arrow-back-circle">
-          <ArrowBackRoundedIcon onClick={handleReturnClick} className="arrow-back"/>
-        </div>
-      </div>
-    </section>
+    <>
+      {loading === false ? (
+        <section className="single-repo-container">
+          <div>
+            <div className="single-repo-wrapper">
+              <h2 className="repo-fullname">{repoDetails.full_name}</h2>
+              <Paper className={classes.root} elevation={3}>
+                  <Typography className="repo-owner"  color="textSecondary" gutterBottom>author: {repoDetails.owner.login}</Typography>
+                  {repoDetails.description &&
+                    <Typography className="repo-description" variant="body2" component="p">{repoDetails.description}</Typography>
+                  }
+                  <div className="stargazing-container">
+                    <div className="stargazing-box">
+                      <StarIcon/>
+                      <Typography className="repo-details" variant="body2" component="p">{repoDetails.stargazers_count}</Typography>
+                    </div>
+                    <Typography className="repo-details" variant="body2" component="p">Language: {repoDetails.language}</Typography>
+                  </div> 
+                  <div className="github-icon">
+                    <a className="github-icon-link" href={`${repoDetails.html_url}`}><GitHubIcon/></a>
+                  </div>       
+              </Paper>
+            </div>
+            <div className="arrow-back-circle">
+              <ArrowBackRoundedIcon onClick={handleReturnClick} className="arrow-back"/>
+            </div>
+          </div>
+        </section> ) : (
+          <Loading/>
+        )
+      }
+    </>
   )
   } else {
     return null
